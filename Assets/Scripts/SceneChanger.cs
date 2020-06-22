2ef4e2a26dc05c633df966e6e56a6bd2aa911cd7
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-06-22 13:52:25 +0300

    Menu- aprox

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+    public void settingsMenu()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+    public void goToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
