c5057bcd35bc02e977e2eb0c56a5df5d0209e9e8
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-06-23 10:36:48 +0300

    Volume+volumesettings

---
+++ Assets/Scripts/VolumeChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeChanger : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        if(volume==-35)
+        { audioMixer.SetFloat("volume", -80); }
+        else 
+        audioMixer.SetFloat("volume", volume);
+        
+    }
+}
