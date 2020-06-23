22430c67eac89db768a5100c6bb81d7c7d70e9dc
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-06-23 08:08:47 +0300

    lil changes

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -7,19 +7,23 @@ public class SceneChanger : MonoBehaviour
 {
     public void startGame()
     {
-        SceneManager.LoadScene("StartScene");
+        SceneManager.LoadScene("2StartScene");
     }
     public void playGame()
     {
-        SceneManager.LoadScene("MapScene");
+        SceneManager.LoadScene("4MapScene");
     }
     public void settingsMenu()
     {
-        SceneManager.LoadScene("SettingsScene");
+        SceneManager.LoadScene("3SettingsScene");
     }
     public void goToMenu()
     {
-        SceneManager.LoadScene("MenuScene");
+        SceneManager.LoadScene("1MenuScene");
+    }
+    public void goToCredits()
+    {
+        SceneManager.LoadScene("5CreditsScene");
     }
     public void quit()
     {
