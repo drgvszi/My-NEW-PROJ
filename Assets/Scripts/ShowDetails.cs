0520fc6696485edde7d3393d58ab092d4073cf54
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-06-25 20:27:04 +0300

    Peace at Night v.0.0.0.0.0.0.2

---
+++ Assets/Scripts/ShowDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDetails : MonoBehaviour
+{
+    public GameObject obj;
+    public int ok=0;
+
+    public void ShowHide()
+    {
+        if (ok == 0)
+        {
+            obj.SetActive(true);
+            ok = 1;
+        }
+        else
+        {
+            obj.SetActive(false);
+            ok = 0;
+        }
+
+    }
+   
+}
--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -7,18 +7,21 @@ public class Teleport : MonoBehaviour
     public GameObject obj;
     public GameObject obj1;
     public Collider2D coll;
-    public int ok = 0;
-    void Update()
-    {
-        coll.isTrigger = true; 
-    }
+    public Collider2D coll1;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         obj.SetActive(false);
         obj1.SetActive(true);
         coll.isTrigger = false;
+        coll1.isTrigger = true;
     }
-   
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        coll.isTrigger = true;
+        coll1.isTrigger = false;
+    }
+
 
 
 }
