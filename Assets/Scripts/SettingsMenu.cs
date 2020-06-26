0ee1390f2cc2839a0a0f2e6f2163b5febe33e5dc
drgvszi <33577111+drgvszi@users.noreply.github.com>
2020-06-26 10:21:32 +0300

    Settings

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer volMixer;
+    public Slider volSlider;
+    public Dropdown resDrop;
+    public Toggle fullScreenToggle;
+    private int screenInt;
+    Resolution[] resolutions;
+    private bool isFullscreen = false;
+    const string prefname = "optionvalue";
+    const string resname = "resolutionoption";
+    void Awake()
+    {
+        screenInt = PlayerPrefs.GetInt("togglestate");
+        if (screenInt == 1)
+        {
+            isFullscreen = true;
+            fullScreenToggle.isOn = true;
+        }
+        else
+        {
+            fullScreenToggle.isOn = false;
+        }
+    
+        resDrop.onValueChanged.AddListener(new UnityAction<int>(index =>
+        {
+        PlayerPrefs.SetInt(resname, resDrop.value);
+        PlayerPrefs.Save();
+        }));
+    }
+    // Update is called once per frame
+    void Start()
+    {
+        volSlider.value = PlayerPrefs.GetFloat("MVolume", 1f);
+        volMixer.SetFloat("volume", PlayerPrefs.GetFloat("MVolume"));
+        resolutions = Screen.resolutions;
+        resDrop.ClearOptions();
+        List<string> options = new List<string>();
+        int currentresID = 0;
+        for(int i=0;i<resolutions.Length;i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+               resolutions[i].height == Screen.currentResolution.height &&
+               resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentresID = i;
+            }
+        }
+        resDrop.AddOptions(options);
+        resDrop.value = PlayerPrefs.GetInt(resname, currentresID);
+        resDrop.RefreshShownValue(); 
+    }
+    public void SetResolution(int resolutionID)
+    {
+        Resolution resolution = resolutions[resolutionID];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void ChangeVol(float volume)
+    {
+        
+        if (volume == -35)
+        {
+            PlayerPrefs.SetFloat("MVolume", -80);
+            volMixer.SetFloat("volume", PlayerPrefs.GetFloat("MVolume"));
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("MVolume", volume);
+            volMixer.SetFloat("volume", PlayerPrefs.GetFloat("MVolume"));
+        }
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        if(isFullscreen==false)
+        {
+            PlayerPrefs.SetInt("togglestate", 0);
+        }
+        else
+        {
+            isFullscreen = false;
+            PlayerPrefs.SetInt("togglestate", 1);
+        }
+    }
+}
---
+++ Assets/Scripts/ShowMenuSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMenuSettings : MonoBehaviour
+{
+    public GameObject obj;
+    public GameObject obj1;
+
+    public void ShowSettings()
+    {
+        obj.SetActive(true);
+        obj1.SetActive(false);
+    }
+    public void ShowMenuButtons()
+    {
+        obj.SetActive(false);
+        obj1.SetActive(true);
+    }
+
+}
